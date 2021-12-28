cba9d4baabdcf04f15c01facd66ea957476db0e8
Igor Angelchev <angel4ev2@gmail.com>
2021-12-28 12:39:22 +0200

    add dist

---
+++ precache-manifest.6ea4c111984059ead20d768cc3397373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7770e3e44d447d3ad342",
+    "url": "/teeter-totter-test/css/app.a69ac971.css"
+  },
+  {
+    "revision": "70113d2b8d07d71d56b5",
+    "url": "/teeter-totter-test/css/chunk-vendors.e630f609.css"
+  },
+  {
+    "revision": "5b3c97528231cb5c9811d9982a26f0df",
+    "url": "/teeter-totter-test/index.html"
+  },
+  {
+    "revision": "7770e3e44d447d3ad342",
+    "url": "/teeter-totter-test/js/app.a203beae.js"
+  },
+  {
+    "revision": "70113d2b8d07d71d56b5",
+    "url": "/teeter-totter-test/js/chunk-vendors.1bae4f55.js"
+  },
+  {
+    "revision": "9a171fe8a02011fd105b1700bcdd52c3",
+    "url": "/teeter-totter-test/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/teeter-totter-test/robots.txt"
+  }
+]);
